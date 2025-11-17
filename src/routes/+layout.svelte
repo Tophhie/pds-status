8f10ea97b08ef093092f989a6d1745f79e05b009
Chris Greenacre <chris.greenacre@tophhie.co.uk>
2025-11-17 08:47:07 +0000

    chore: replace SVG favicon with PNG version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Tophhie Social Server Status</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
